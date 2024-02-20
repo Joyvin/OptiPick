b6d99f80af400b7443785bd17f7bd02728830fa5
Alstudd <alstonsoares17@gmail.com>
2024-02-20 18:24:43 +0530

    Added landing page and chatbot UI

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,10 +1,19 @@
-import Image from "next/image";
-import Team from "../components/Team"
+import Encryption from "@/components/main/Encryption";
+import Hero from "@/components/main/Hero";
+import Projects from "@/components/main/Projects";
+import Skills from "@/components/main/Skills";
+import Team from "@/components/Team";
 
 export default function Home() {
   return (
-    <>
-    <Team/>
-    </>
+    <main className="h-full w-full">
+      <div className="flex flex-col gap-20">
+        <Hero />
+        <Skills />
+        <Encryption />
+        <Projects />
+      </div>
+    </main>
   );
 }
+
