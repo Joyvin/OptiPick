87bba1dcf325c571dd42055f7fb534bbcb42cdf7
Alstudd <alstonsoares17@gmail.com>
2024-02-20 21:04:14 +0530

    just some noob changes

--- web/src/components/Chatbot.js
+++ web/src/components/Chatbot.js
@@ -28,13 +28,13 @@ async function getResponse(prompt) {
 export const userDiv = (data) => {
   return `
   <!-- User Chat -->
-          <div class="flex items-center gap-2 justify-start my-4">
+          <div className="flex items-center gap-2 justify-start my-4">
             <img
               src="user.jpg"
               alt="user icon"
-              class="w-10 h-10 rounded-full"
+              className="w-10 h-10 rounded-full"
             />
-            <p class="bg-transparent border-2 border-white text-black px-4 py-2 rounded-md shadow-md">
+            <p className="bg-transparent border-2 border-white text-black px-4 py-2 rounded-md shadow-md">
               ${data}
             </p>
           </div>
@@ -45,14 +45,14 @@ export const userDiv = (data) => {
 export const aiDiv = (data) => {
   return `
   <!-- AI Chat -->
-          <div class="flex gap-2 justify-end pl-[130px] my-4">
-            <pre class="bg-transparent border-2 border-white text-black px-4 py-2 rounded-md shadow-md whitespace-pre-wrap">
+          <div className="flex gap-2 justify-end pl-[130px] my-4">
+            <pre className="bg-transparent border-2 border-white text-black px-4 py-2 rounded-md shadow-md whitespace-pre-wrap">
               ${data}
             </pre>
             <img
               src="chat-bot.jpg"
               alt="user icon"
-              class="w-10 h-10 rounded-full"
+              className="w-10 h-10 rounded-full"
             />
           </div>
   `;
@@ -106,18 +106,18 @@ const Chatbot = () => {
         <div className="container mx-auto sm:px-4 mt-14">
 
           <div className="z-[20] relative flex flex-col min-w-0 break-words border bg-white border-1 border-gray-300 shadow-md rounded-xl">
-            <div class="flex align-items-center gap-3 mt-6 ml-6">
-              <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-bot"><path d="M12 8V4H8" /><rect width="16" height="12" x="4" y="8" rx="2" /><path d="M2 14h2" /><path d="M20 14h2" /><path d="M15 13v2" /><path d="M9 13v2" /></svg>
+            <div className="flex align-items-center gap-3 mt-6 ml-6">
+              <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-bot"><path d="M12 8V4H8" /><rect width="16" height="12" x="4" y="8" rx="2" /><path d="M2 14h2" /><path d="M20 14h2" /><path d="M15 13v2" /><path d="M9 13v2" /></svg>
               <h5 className="text-lg font-medium text-black mb-0">
                 OptiPick AI Chatbot
               </h5>
             </div>
             <div className="flex-auto p-6">
               <div className="flex flex-col">
-                <div id="chat-container" ref={chatArea} class="max-h-[67vh]"></div>
+                <div id="chat-container" ref={chatArea} className="max-h-[67vh]"></div>
                 <div className="w-full p-2 flex border border-black rounded-full py-2 px-4">
                   <form onSubmit={handleSubmit} action="" method="post" className="w-full flex px-3" id="chat-form" ref={chatForm} onKeyUp={handleKeyup}>
-                    <input placeholder="Ask your question?" type="text" name="" ref={userMessage} id="prompt" class="w-full p-1 pb-0 outline-none border-none bg-transparent text-black" />
+                    <input placeholder="Ask your question?" type="text" name="" ref={userMessage} id="prompt" className="w-full p-1 pb-0 outline-none border-none bg-transparent text-black" />
                     <button className="text-black" type="submit">Send</button>
                   </form>
                 </div>
--- web/src/components/main/Footer.tsx
+++ web/src/components/main/Footer.tsx
@@ -8,66 +8,97 @@ import {
 } from "react-icons/rx";
 
 import { FaYoutube } from "react-icons/fa";
+import { Sparkles } from "lucide-react";
 
 const Footer = () => {
   return (
-    <div className="w-full h-full bg-transparent text-gray-200 shadow-lg p-[15px] ">
-        <div className="w-full flex flex-col items-center justify-center m-auto">
-            <div className="w-full h-full flex flex-row items-center justify-around flex-wrap">
+    // <div className="w-full h-full bg-transparent text-gray-200 shadow-lg p-[15px] ">
+    //     <div className="w-full flex flex-col items-center justify-center m-auto">
+    //         <div className="w-full h-full flex flex-row items-center justify-around flex-wrap">
                 
 
-                <div className="min-w-[200px] h-auto flex flex-col items-center justify-start">
-                    <div className="font-bold text-[16px]">Community</div>
-                    <p className="flex flex-row items-center my-[15px] cursor-pointer">
-                        <FaYoutube />
-                        <span className="text-[15px] ml-[6px]">Youtube</span>    
-                    </p>
-                    <p className="flex flex-row items-center my-[15px] cursor-pointer">
-                        <RxGithubLogo />
-                        <span className="text-[15px] ml-[6px]">Github</span>    
-                    </p>
-                    <p className="flex flex-row items-center my-[15px] cursor-pointer">
-                        <RxDiscordLogo />
-                        <span className="text-[15px] ml-[6px]">Discord</span>    
-                    </p>
-                </div>
-                <div className="min-w-[200px] h-auto flex flex-col items-center justify-start">
-                    <div className="font-bold text-[16px]">Social Media</div>
-                    <p className="flex flex-row items-center my-[15px] cursor-pointer">
-                        <FaYoutube />
-                        <span className="text-[15px] ml-[6px]">Instagram</span>    
-                    </p>
-                    <p className="flex flex-row items-center my-[15px] cursor-pointer">
-                        <RxGithubLogo />
-                        <span className="text-[15px] ml-[6px]">Twitter</span>    
-                    </p>
-                    <p className="flex flex-row items-center my-[15px] cursor-pointer">
-                        <RxDiscordLogo />
-                        <span className="text-[15px] ml-[6px]">Linkedin</span>    
-                    </p>
-                </div>
-                <div className="min-w-[200px] h-auto flex flex-col items-center justify-start">
-                    <div className="font-bold text-[16px]">About</div>
-                   <p className="flex flex-row items-center my-[15px] cursor-pointer">
+    //             <div className="min-w-[200px] h-auto flex flex-col items-center justify-start">
+    //                 <div className="font-bold text-[16px]">Community</div>
+    //                 <p className="flex flex-row items-center my-[15px] cursor-pointer">
+    //                     <FaYoutube />
+    //                     <span className="text-[15px] ml-[6px]">Youtube</span>    
+    //                 </p>
+    //                 <p className="flex flex-row items-center my-[15px] cursor-pointer">
+    //                     <RxGithubLogo />
+    //                     <span className="text-[15px] ml-[6px]">Github</span>    
+    //                 </p>
+    //                 <p className="flex flex-row items-center my-[15px] cursor-pointer">
+    //                     <RxDiscordLogo />
+    //                     <span className="text-[15px] ml-[6px]">Discord</span>    
+    //                 </p>
+    //             </div>
+    //             <div className="min-w-[200px] h-auto flex flex-col items-center justify-start">
+    //                 <div className="font-bold text-[16px]">Social Media</div>
+    //                 <p className="flex flex-row items-center my-[15px] cursor-pointer">
+    //                     <FaYoutube />
+    //                     <span className="text-[15px] ml-[6px]">Instagram</span>    
+    //                 </p>
+    //                 <p className="flex flex-row items-center my-[15px] cursor-pointer">
+    //                     <RxGithubLogo />
+    //                     <span className="text-[15px] ml-[6px]">Twitter</span>    
+    //                 </p>
+    //                 <p className="flex flex-row items-center my-[15px] cursor-pointer">
+    //                     <RxDiscordLogo />
+    //                     <span className="text-[15px] ml-[6px]">Linkedin</span>    
+    //                 </p>
+    //             </div>
+    //             <div className="min-w-[200px] h-auto flex flex-col items-center justify-start">
+    //                 <div className="font-bold text-[16px]">About</div>
+    //                <p className="flex flex-row items-center my-[15px] cursor-pointer">
                      
-                        <span className="text-[15px] ml-[6px]">Become Sponsor</span>    
-                    </p>
-                    <p className="flex flex-row items-center my-[15px] cursor-pointer">
+    //                     <span className="text-[15px] ml-[6px]">Become Sponsor</span>    
+    //                 </p>
+    //                 <p className="flex flex-row items-center my-[15px] cursor-pointer">
                       
-                        <span className="text-[15px] ml-[6px]">Learning about me</span>    
-                    </p>
-                    <p className="flex flex-row items-center my-[15px] cursor-pointer">
+    //                     <span className="text-[15px] ml-[6px]">Learning about me</span>    
+    //                 </p>
+    //                 <p className="flex flex-row items-center my-[15px] cursor-pointer">
                   
-                        <span className="text-[15px] ml-[6px]">alstonsoares17@gmail.com</span>    
-                    </p>
-                </div>
-            </div>
+    //                     <span className="text-[15px] ml-[6px]">alstonsoares17@gmail.com</span>    
+    //                 </p>
+    //             </div>
+    //         </div>
 
-            <div className="mb-[20px] text-[15px] text-center">
-                &copy; Alstudd
-            </div>
+    //         <div className="mb-[20px] text-[15px] text-center">
+    //             &copy; Alstudd
+    //         </div>
+    //     </div>
+    // </div>
+
+
+<footer className="bg-transparent rounded-lg shadow m-4 relative z-[20]">
+    <div className="w-full max-w-screen-xl mx-auto p-4">
+        <div className="sm:flex sm:items-center sm:justify-between">
+            <a href="/" className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
+                <Sparkles className="text-white"/>
+                <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">OptiPick</span>
+            </a>
+            <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
+                <li>
+                    <a href="/dashboard" className="hover:underline me-4 md:me-6">Dashboard</a>
+                </li>
+                <li>
+                    <a href="#" className="hover:underline me-4 md:me-6">About</a>
+                </li>
+                <li>
+                    <a href="/chatbot" className="hover:underline me-4 md:me-6">Chatbot</a>
+                </li>
+                <li>
+                    <a href="#" className="hover:underline">Contact</a>
+                </li>
+            </ul>
         </div>
+        <hr className="my-2 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-3" />
+        <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="/" className="hover:underline">OptiPick</a>. All Rights Reserved.</span>
     </div>
+</footer>
+
+
   )
 }
 
