a7c969d9d0406f10395ef77c5da55e8648c52059
Joyvin <joymendonca710@gmail.com>
2024-02-20 23:18:59 +0530

    ext connection

--- web/src/app/endpoint/route.js
+++ web/src/app/endpoint/route.js
@@ -1,18 +1,21 @@
 import { NextResponse } from "next/server";
-export const dynamic = 'force-dynamic' // defaults to auto
+export const dynamic = "force-dynamic"; // defaults to auto
 
 export async function POST(request) {
-    const { url: itemUrl } = await request.json();
-    
-    console.log('URL:', itemUrl);
-  
-    return new Response('Hello, Next.js!', {
-        status: 200,
-        headers: {
-            'Access-Control-Allow-Origin': '*',
-            'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
-            'Access-Control-Allow-Headers': 'Content-Type, Authorization',
-        },
-    })
-}
+	const { url: itemUrl } = await request.json();
 
+	let res = await axios.post("http://localhost:3000/api/scrape", {
+		url: itemUrl,
+	});
+
+	console.log(res);
+
+	return new Response(res, {
+		status: 200,
+		headers: {
+			"Access-Control-Allow-Origin": "*",
+			"Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+			"Access-Control-Allow-Headers": "Content-Type, Authorization",
+		},
+	});
+}
