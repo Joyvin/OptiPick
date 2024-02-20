a76b5fdc6c236d40023c4f8f8a80a2e73c81a01b
ZaneFerns360 <zane.ferns360@gmail.com>
2024-02-20 12:44:07 +0530

    added extension

---
+++ sentimental/src/popup.tsx
@@ -0,0 +1,13 @@
+import { CountButton } from "~features/count-button"
+
+import "~style.css"
+
+function IndexPopup() {
+  return (
+    <div className="flex items-center justify-center h-48 w-64">
+      <CountButton />
+    </div>
+  )
+}
+
+export default IndexPopup
