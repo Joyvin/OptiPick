1c83ba4c6931ff7524c54a4bda98ea10ddde0eed
AlvinDHacker <alvindsouza2204@gmail.com>
2024-02-21 00:53:47 +0530

    Edited Home

--- web/next.config.mjs
+++ web/next.config.mjs
@@ -11,6 +11,22 @@ const nextConfig = {
 			},
 		];
 	},
+	images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'lh3.googleusercontent.com',
+                port: '',
+                pathname: '/**',
+            },
+            {
+                protocol: 'https',
+                hostname: 'images.unsplash.com',
+                port: '',
+                pathname: '/**',
+            },
+        ],
+    },
 };
 
 export default nextConfig;
