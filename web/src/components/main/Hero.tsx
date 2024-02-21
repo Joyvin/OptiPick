fffcd025ac68dc129cc5dbc9e68ef8c6d1d13420
Alstudd <alstonsoares17@gmail.com>
2024-02-21 13:13:09 +0530

    minor change

--- web/src/components/main/Hero.tsx
+++ web/src/components/main/Hero.tsx
@@ -8,7 +8,7 @@ const Hero = () => {
         autoPlay
         muted
         loop
-        className="rotate-180 absolute top-[-340px] h-full w-full left-0 z-[1] object-cover"
+        className="rotate-180 absolute top-[-340px] h-full w-full -left-10 z-[1] object-cover"
       >
         <source src="/blackhole.webm" type="video/webm" />
       </video>
